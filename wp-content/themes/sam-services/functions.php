7a498894b4e58a420346bb2546dbcbe654d6da50
Pea <pea@misfist.com>
2014-09-26 13:10:32 -0400

    Updated styles. Added new slider.

--- wp-content/themes/sam-services/functions.php
+++ wp-content/themes/sam-services/functions.php
@@ -3,8 +3,8 @@
 /************* Add Slug to Body Class *****************/
 
 // Add specific CSS class by filter
-add_filter('body_class','glocal_network_class_names');
-function glocal_network_class_names($classes) {
+add_filter('body_class','sam_services_class_names');
+function sam_services_class_names($classes) {
 	// add 'class-name' to the $classes array
 	global $post; 
 	$post_slug_class = $post->post_name; 
@@ -13,8 +13,38 @@ function glocal_network_class_names($classes) {
 	return $classes;
 }
 
+/************* Sidebars *****************/
+
+function sam_services_remove_widgets(){
+
+	// Unregister some sidebars
+	unregister_sidebar( 'gallery-widget' );
+	unregister_sidebar( 'colophon-widget' );
+	unregister_sidebar( 'left-sidebar-half' );
+	unregister_sidebar( 'right-sidebar-half' );
+	unregister_sidebar( 'left-sidebar' );
+	unregister_sidebar( 'main-sidebar' );
+
+}
+add_action( 'widgets_init', 'sam_services_remove_widgets', 11 );
 
 
 
+function sam_services_add_widgets() {
+
+	// register some sidebars
+
+	register_sidebar( array(
+		'name'          => __( 'Home Widget 4', 'sam-services' ),
+		'description'   => __( '4th widget on homepage', 'sam-services' ),
+		'id'            => 'home-widget-4',
+		'before_title'  => '<div class="widget-title"><h3>',
+		'after_title'   => '</h3></div>',
+		'before_widget' => '<div id="%1$s" class="widget-wrapper %2$s">',
+		'after_widget'  => '</div>'
+	) );
+
+}
+add_action( 'widgets_init', 'sam_services_add_widgets' );
 
 ?>

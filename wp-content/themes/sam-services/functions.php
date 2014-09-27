16ab3130333da08965ba8a690e61784f8ca35f43
Pea <pea@misfist.com>
2014-09-27 15:05:08 -0400

    Removed custom header because of error.

--- wp-content/themes/sam-services/functions.php
+++ wp-content/themes/sam-services/functions.php
@@ -194,7 +194,7 @@ add_action( 'wp_enqueue_scripts', 'sam_services_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
+// require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.

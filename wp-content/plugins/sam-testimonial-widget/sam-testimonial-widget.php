c99d0ece2023be7585a8a2408e275cb783973678
Pea <pea@misfist.com>
2014-09-30 19:34:25 -0400

    Changed name Changed name of testimonials widget because WP thinks it's something else...

--- wp-content/plugins/sam-testimonial-widget/sam-testimonial-widget.php
+++ wp-content/plugins/sam-testimonial-widget/sam-testimonial-widget.php
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License along with this program; if not, write 
  * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
- * @package    SAM Testimonials_Widget
+ * @package    SAM_Testimonial_Widget
  * @since      0.1
  * @author     Pea
  * @license    http://www.gnu.org/licenses/gpl-2.0.html
